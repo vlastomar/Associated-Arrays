da8a4c785c8c2702a9feae2cf10a6329a12340b3
VSM <vlad24@abv.bg>
2020-08-01 09:23:16 +0300

    Create WordFilter.java

---
+++ WordFilter.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class WordFilter {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String[] words = Arrays.stream(scan.nextLine().split("\\s+"))
+                .filter(e -> e.length() % 2 == 0)
+                .toArray(String[]::new);
+        for( String word : words){
+            System.out.println(word);
+        }
+    }
+}
